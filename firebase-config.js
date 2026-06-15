c4d25aeb44f407de351eae58c5c759a8f06d7a27
waldomaker <waldoemersonlawrencea@gmail.com>
2026-06-15 10:37:04 -0400

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.firebaseConfig = {
-  apiKey: "AIzaSyAl8IeYwp3tDp2U-EeQv0brkLzrgC43xGM",
-  authDomain: "manmie-bar.firebaseapp.com",
-  databaseURL: "https://manmie-bar-default-rtdb.firebaseio.com",
-  projectId: "manmie-bar",
-  storageBucket: "manmie-bar.firebasestorage.app",
-  messagingSenderId: "1002274107668",
-  appId: "1:1002274107668:web:656d671bf9c4bbb6079201"
-};
